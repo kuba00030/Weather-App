78d86e79164c5d310ba21168ad7a18e51af2db78
unknown <kuba00030@interia.pl>
2023-01-25 13:21:02 +0100

    added container & widdget for hourly weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Mid } from "./components/mid/Mid";
 import Top from "./components/top/Top";
 function App() {
   return (
     <div className="App">
       <Top />
+      <Mid />
     </div>
   );
 }
---
+++ src/components/widgets/WidgetHourlyWeatherDetails.tsx
@@ -0,0 +1,13 @@
+const HourlyWeatherDetails = () => {
+  return (
+    <div className="Hourly-weather-details">
+      <div className="HDetails">
+        <span className="Hour">15:00</span>
+        <div className="HImg">IMG</div>
+        <span className="HTemp">21</span>
+      </div>
+    </div>
+  );
+};
+
+export { HourlyWeatherDetails };
